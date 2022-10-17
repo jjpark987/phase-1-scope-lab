e7ca77f730a7aa77578ca05070dc3300caffaebf
Jason Park <jjpark987@gmail.com>
2022-10-17 10:27:50 -0700

    commit tree f52a15a0dacd43b94bbceb0bd815e5cf0ab9773b

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+var customerName = `bob`;
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+var bestCustomer;
+
+function setBestCustomer() {
+    bestCustomer = `not bob`;
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    return bestCustomer = `maybe bob`;
+}
+
+const leastFavoriteCustomer = `teddy`;
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = `still teddy`;
+}
